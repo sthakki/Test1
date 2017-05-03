1bd8b7fe2cc3232aeada0eed8ed7f7274b779ed7
sthakki <soumyahakki@gmail.com>
2017-05-03 14:10:42 +0100

    change

--- src/demo6.java
+++ src/demo6.java
@@ -3,5 +3,9 @@ public class demo6
 {
 	public static void main(String[] args) {
 		System.out.println("happy.....");
+		
+		System.out.println("happy.....");
+		System.out.println("happy.....");
+		
 	}
 }
